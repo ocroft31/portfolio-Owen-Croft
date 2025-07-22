ba9f2d2a10c69a3e51bca737a3cc1a42cc328794
ocroft31 <owencroft@gmail.com>
2025-07-22 16:40:26 +0000

    Deploying to gh-pages from @ ocroft31/portfolio-Owen-Croft@2d37f2d76692559dda606ae6adfcb25a282257f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/portfolio-Owen-Croft/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/portfolio-Owen-Croft/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
