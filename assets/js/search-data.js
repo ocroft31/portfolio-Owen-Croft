a22399fbb4f7e2c5ee44fa2ca3da5abf9f38e723
ocroft31 <owencroft@gmail.com>
2025-07-23 10:59:54 +0000

    Deploying to gh-pages from @ ocroft31/portfolio-Owen-Croft@17c746e12e0cf698b70471324693bc3b246f4226 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of projects I have worked on/completed.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio-Owen-Croft/projects/";
