44910343ab8d26a67ad133af23dfe00b6b7f0229
ocroft31 <owencroft@gmail.com>
2025-07-23 10:57:41 +0000

    Deploying to gh-pages from @ ocroft31/portfolio-Owen-Croft@f68749df92a5ed17751aeecc3ace276b0530376e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "Python code for a web scraper of TransferMarkt.com using BS4 and Requests",
           section: "Projects",handler: () => {
               window.location.href = "/portfolio-Owen-Croft/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio-Owen-Croft/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
